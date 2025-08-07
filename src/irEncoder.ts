4bdf52e0697fe590373daa081ebeab82c68d588c
DG-7D <96519093+DG-7D@users.noreply.github.com>
2025-08-07 17:41:53 +0900

    おやすみ修正

--- src/irEncoder.ts
+++ src/irEncoder.ts
@@ -1,4 +1,4 @@
-type Mode = "Heat" | "Auto" | "Cool" | "Dry" | "Fan";
+export type Mode = "Heat" | "Auto" | "Cool" | "Dry" | "Fan";
 const modeBits: Record<Mode, number> = {
     Heat: 0b000,
     Auto: 0b001,
@@ -7,7 +7,7 @@ const modeBits: Record<Mode, number> = {
     Fan: 0b100,
 };
 
-type Volume = "Silent" | "1" | "2" | "3" | "4" | "Powerful" | "Auto";
+export type Volume = "Silent" | "1" | "2" | "3" | "4" | "Powerful" | "Auto";
 const volumeBits: Record<Volume, number> = {
     Silent: 0b0000001,
     "1": 0b0001010,
@@ -18,7 +18,7 @@ const volumeBits: Record<Volume, number> = {
     Auto: 0b1111111,
 };
 
-type TemperatureAbsolute = 16 | 16.5 | 17 | 17.5 | 18 | 18.5 | 19 | 19.5
+export type TemperatureAbsolute = 16 | 16.5 | 17 | 17.5 | 18 | 18.5 | 19 | 19.5
     | 20 | 20.5 | 21 | 21.5 | 22 | 22.5 | 23 | 23.5 | 24 | 24.5
     | 25 | 25.5 | 26 | 26.5 | 27 | 27.5 | 28 | 28.5 | 29 | 29.5
     | 30;
@@ -26,7 +26,7 @@ function temperatureAbsoluteToNumber(temp: TemperatureAbsolute): number {
     return (temp - 16) * 10;
 }
 
-type TemperatureRelative = -2 | -1 | 0 | 1 | 2;
+export type TemperatureRelative = -2 | -1 | 0 | 1 | 2;
 const temperatureRelativeBits: Record<TemperatureRelative, number> = {
     "-2": 0b110,
     "-1": 0b101,
@@ -35,30 +35,30 @@ const temperatureRelativeBits: Record<TemperatureRelative, number> = {
     "2": 0b010,
 };
 
-type Hour = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+export type Hour = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
     | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19
     | 20 | 21 | 22 | 23;
-type Minute = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+export type Minute = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
     | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19
     | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29
     | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39
     | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49
     | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59;
 
-type DirectionVertical = "Auto" | "Stop";
+export type DirectionVertical = "Auto" | "Stop";
 const directionVerticalBits: Record<DirectionVertical, number> = {
     Auto: 0b0,
     Stop: 0b1,
 };
 
-type DirectionHorizontal = "Auto" | "Stop";
+export type DirectionHorizontal = "Auto" | "Stop";
 const directionHorizontalBits: Record<DirectionHorizontal, number> = {
     Auto: 0b01,
     Stop: 0b10,
 };
 
-type ButtonOthers = "Dry" | "Fan" | "Cancel" | "PowerSave";
-type Button = "Auto" | "Stop" | "Cool" | "Heat" | "TempDown" | "TempUp" | "Volume" | "DirectionVertical" | "DirectionHorizontal" | "Sleep" | "Clean" | "ECO" | "OnTimer" | "OffTimer" | "Other";
+export type ButtonOthers = "Dry" | "Fan" | "Cancel" | "PowerSave";
+export type Button = "Auto" | "Stop" | "Cool" | "Heat" | "TempDown" | "TempUp" | "Volume" | "DirectionVertical" | "DirectionHorizontal" | "Sleep" | "Clean" | "ECO" | "OnTimer" | "OffTimer" | "Other";
 const buttonBits: Record<Button, number> = {
     Other: 0b00000,
     Stop: 0b00001,
@@ -77,14 +77,14 @@ const buttonBits: Record<Button, number> = {
     OffTimer: 0b11101,
 };
 
-type CheckCode = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+export type CheckCode = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
     | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19
     | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29
     | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39
     | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49;
 
 
-interface State {
+export interface State {
     running: boolean;
     mode: Mode;
     volume: Volume;
@@ -117,7 +117,7 @@ export function encode(state: State, button: Button): Uint8Array {
     buf[6] = temperatureAbsoluteToNumber(state.temperatureAbsolute);
     buf[7] = temperatureRelativeBits[state.temperatureRelative];
     buf[8] = 0b00000000;
-    buf[9] = (state.sleep ? 1 : 0) << 1;
+    buf[9] = (state.sleep ? 1 : 0) << 2;
     buf[10] = (state.powerSave ? 1 : 0) << 7 | state.hour;
     buf[11] = state.minute;
     buf[12] = (state.cleaning ? 1 : 0) << 6 | (state.offTimer ? 1 : 0) << 5 | state.offTimerHour;
